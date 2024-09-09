ff382203d7ce83fd277ce0b5a09326e1fba490b3
aernest njuki <74731728+Aernestnjuki@users.noreply.github.com>
2024-09-09 23:18:05 +0300

    bookmarks

---
+++ src/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import os
+from src.auth import auth
+from src.booKMark import bookmarks
+from src.database import db
+
+
+def create_app(test_config=None):
+    app = Flask(
+        __name__,
+        instance_relative_config=True # tells flask that there will be more configurations
+    )
+
+    if test_config is None:
+
+        app.config.from_mapping(
+            SECRET_KEY=os.environ.get('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app.config.from_mapping(test_config)
+
+
+    db.init_app(app)
+
+    app.register_blueprint(auth)
+    app.register_blueprint(bookmarks)
+
+    return app
